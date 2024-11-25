0c18dc62142246ace85f4a44529b4acf6a957b81
Johann <154441181+johann-l@users.noreply.github.com>
2024-11-25 22:53:29 +0530

    new stuff 5

--- src/review/FieldSet.js
+++ src/review/FieldSet.js
@@ -5,10 +5,10 @@ export default function FieldSet({AppName}){
         <fieldset>
             <legend>Review the {AppName} App</legend><br />
 
-            <label htmlFor="{AppName}Rating">Rating (1-5):</label>
-            <input type="number" id="{AppName}Rating" name="{AppName}Rating" min="1" max="5" required /><br /><br />
-            <label htmlFor="{AppName}Feedback">Feedback:</label><br />
-            <textarea id="{AppName}Feedback" name="{AppName}Feedback" rows="4" cols="50" placeholder="What did you like or dislike about the {AppName} App?"></textarea><br /><br />
+            <label htmlFor={`${AppName}Rating`}>Rating (1-5):</label>
+            <input type="number" id={`${AppName}Rating`} name={`${AppName}Rating`} min="1" max="5" style={{fontSize: "19px"}}required /><br /><br />
+            <label htmlFor={`${AppName}Feedback`}>Feedback:</label><br />
+            <textarea id={`${AppName}Feedback`} name={`${AppName}Feedback`} rows="4" cols="50" placeholder={`What did you like or dislike about the ${AppName} App?`}></textarea><br /><br />
         </fieldset>
-    );
+    ); 
 }
