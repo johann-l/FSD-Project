e135c558ac58f0c75028a45b3f47aa3efd375099
Johann <154441181+johann-l@users.noreply.github.com>
2024-11-25 22:25:13 +0530

    new stuff 4

---
+++ src/review/FieldSet.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function FieldSet({AppName}){
+    return(
+        <fieldset>
+            <legend>Review the {AppName} App</legend><br />
+
+            <label htmlFor="{AppName}Rating">Rating (1-5):</label>
+            <input type="number" id="{AppName}Rating" name="{AppName}Rating" min="1" max="5" required /><br /><br />
+            <label htmlFor="{AppName}Feedback">Feedback:</label><br />
+            <textarea id="{AppName}Feedback" name="{AppName}Feedback" rows="4" cols="50" placeholder="What did you like or dislike about the {AppName} App?"></textarea><br /><br />
+        </fieldset>
+    );
+}
--- src/review/review.js
+++ src/review/review.js
@@ -1,25 +1,10 @@
 import '../css-files/review.css'; //css
-import { Link, useNavigate, useLocation } from "react-router-dom"; //routing
-import React, { useEffect } from 'react';
+import { Link, useNavigate } from "react-router-dom"; //routing
+import FieldSet from './FieldSet'; //js
+import React from 'react';
 
 export default function Review(){
     const navi = useNavigate();
-    const loc = useLocation();
-
-    useEffect(() => {
-        if (loc.state?.from === "/calculator") {
-            const handlePopState = () => {
-                navi("/", { replace: true });
-            };
-
-            window.addEventListener("popstate", handlePopState);
-
-            return () => {
-                window.removeEventListener("popstate", handlePopState);
-            };
-        }
-    }, [loc, navi]);
-
     function handleSubmit(e){
         e.preventDefault();
         alert("Thanks for your review!");
@@ -31,24 +16,8 @@ export default function Review(){
             <Link to="/">Go Back</Link>
             <h1>App Review Form</h1>
             <form id="reviewForm" onSubmit={handleSubmit} style={{padding: "10px"}}>
-                <fieldset>
-                    <legend>Review the Calculator App</legend><br />
-
-                    <label htmlFor="calculatorRating">Rating (1-5):</label>
-                    <input type="number" id="calculatorRating" name="calculatorRating" min="1" max="5" required /><br /><br />
-
-                    <label htmlFor="calculatorFeedback">Feedback:</label><br />
-                    <textarea id="calculatorFeedback" name="calculatorFeedback" rows="4" cols="50" placeholder="What did you like or dislike about the Calculator App?"></textarea><br /><br />
-                </fieldset>
-                <fieldset>
-                    <legend>Review the Calendar App</legend><br />
-
-                    <label htmlFor="calendarRating">Rating (1-5):</label>
-                    <input type="number" id="calendarRating" name="calendarRating" min="1" max="5" required /><br /><br />
-
-                    <label htmlFor="calendarFeedback">Feedback:</label><br />
-                    <textarea id="calendarFeedback" name="calendarFeedback" rows="4" cols="50" placeholder="What did you like or dislike about the Calendar App?"></textarea><br /><br />
-                </fieldset>
+                <FieldSet AppName={'Calculator'}/>
+                <FieldSet AppName={'Calendar'}/>
                 <button type="submit">Submit Review</button>
             </form>
         </div>
